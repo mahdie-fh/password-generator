bbc5aa7ff1eb09074bb99ca5b79da177ffa42382
mahdie <mahdiefh9@gmail.com>
2022-05-09 14:12:12 +0430

    add ant design and generate functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'passwoed-generator';
-}
+public fpassword!:string
+
+  public formGroup: FormGroup = new FormGroup({
+    length: new FormControl('', [Validators.required]),
+    big: new FormControl(false),
+    low: new FormControl(false),
+    numbs: new FormControl(false),
+    symb: new FormControl(false),
+  });
+  get controlTitle(): FormControl {
+    return this.formGroup.get('length') as FormControl;
+  }
+  get controlBig(): FormControl {
+    return this.formGroup.get('big') as FormControl;
+  }
+  get controlLow(): FormControl {
+    return this.formGroup.get('low') as FormControl;
+  }
+  get controlNumbs(): FormControl {
+    return this.formGroup.get('numbs') as FormControl;
+  }
+  get controlSymbw(): FormControl {
+    return this.formGroup.get('symb') as FormControl;
+  }
+
+
+  passwordGenerator(length:any, useUpper:any, useNumbers:any, userSymbols:any) {
+    const lowerCharacters = 'asdfghjklzxcvbnmqwertyuiop';
+    const upperCharacters = 'ASDFGHJKLZXCVBNQWERTYUIOP';
+    const numbers = '123456789';
+    const symbols = '!@#$%^&*';
+    let passwordLength = length;
+    let finalCharacters = lowerCharacters;
+    let password = '';
+
+    if (useUpper === true) {
+      finalCharacters = finalCharacters.concat(upperCharacters);
+    }
+
+    if (useNumbers === true) {
+      finalCharacters = finalCharacters.concat(numbers);
+    }
+
+    if (userSymbols === true) {
+      finalCharacters = finalCharacters.concat(symbols);
+    }
+    for (var i = 0; i <= passwordLength; i++) {
+      let randomNumber = Math.floor(Math.random() * finalCharacters.length);
+
+      password += finalCharacters.substring(randomNumber, randomNumber + 1);
+    }
+    console.log(password);
+    this.fpassword=password
+  }
+
+  create() {
+    this.passwordGenerator(this.controlTitle.value, this.controlBig.value, this.controlNumbs.value, this.controlSymbw.value);
+  }}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzCheckboxModule,
+    NzButtonModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
